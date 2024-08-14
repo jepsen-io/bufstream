509593c0fe29e87ce712dd7f60fc25a7186c5764
Kyle Kingsbury <aphyr@aphyr.com>
2024-08-14 17:53:20 -0500

    queue: restrict client to only bufstream nodes.

--- src/jepsen/bufstream/workload/queue.clj
+++ src/jepsen/bufstream/workload/queue.clj
@@ -4,12 +4,14 @@
             [jepsen [checker :as checker]
                     [client :as client]
                     [generator :as gen]
+                    [role :as role]
                     [util :as util]]
             [jepsen.redpanda.workload.queue :as rq]
             [jepsen.tests.kafka :as k]))
 
 (defn workload
-  "Constructs a test workload given CLI options."
+  "Constructs a test workload given CLI options. Options are documented in the
+  Redpanda client and jepsen.tests.kafka/workload docs."
   [opts]
   (assoc (k/workload opts)
-         :client (rq/client)))
+         :client (role/restrict-client (rq/client) :bufstream)))
